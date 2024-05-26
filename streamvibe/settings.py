7c047f0942fafde6f9d2a1e292168f6d540ae9da
Sebastian Henao <henao7730@gmail.com>
2024-05-26 11:44:41 -0500

    Configuracion para el despliegue de la aplicacion

--- streamvibe/settings.py
+++ streamvibe/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
